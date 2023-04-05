09c7f65135dc8a0d7e9850c9765bf37904e60294
Plazo-09 <abayew.arslan@gmail.com>
2023-04-05 16:12:09 +0500

    Finnished

--- my_first_git_project.py
+++ my_first_git_project.py
@@ -1,1 +1,15 @@
-import os
+class Person:
+    def __init__(self, name, age, job, salary):
+        self.name = name
+        self.age = age
+        self.job = job
+        self.__salary = salary
+    
+    def work(self):
+        return f'{self.name} working in {self.job} for {self.__salary}'
+
+class Policman(Person):
+    pass
+
+pol = Policman('Adam', 45, input('Enter a job name: '), 3000)
+print(pol.work())
